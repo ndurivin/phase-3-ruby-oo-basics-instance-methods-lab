731db5456d350399d237ea0a18410ce5bf0e5998
Vinn Nduri <ndurivo@gmail.com>
2022-08-21 06:38:09 +0300

    commit tree da7230d53f6ee4f95be9fd6a7cbeea61f0e20e3c

---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Person
+    def talk
+        puts "Hello World!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+
+    person1 = Person.new
+    person1.talk
+    person1.walk
+end
